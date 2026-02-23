8982be1c2016661c69ab78a35377f266a284243d
fcalla557372-ai <f.calla.557372@umindanao.edu.ph>
2026-02-23 15:12:09 +0800

    Update navigation links to point to assessment_db

--- nav.php
+++ nav.php
@@ -1,9 +1,9 @@
 <?php // nav.php ?>
 <nav class="navbar">
-  <a href="/assessment_beginner/index.php">Dashboard</a>
-  <a href="/assessment_beginner/pages/clients_list.php">Clients</a>
-  <a href="/assessment_beginner/pages/services_list.php">Services</a>
-  <a href="/assessment_beginner/pages/bookings_list.php">Bookings</a>
-  <a href="/assessment_beginner/pages/tools_list_assign.php">Tools</a>
-  <a href="/assessment_beginner/pages/payments_list.php">Payments</a>
+  <a href="/assessment_db/index.php">Dashboard</a>
+  <a href="/assessment_db/pages/clients_list.php">Clients</a>
+  <a href="/assessment_db/pages/services_list.php">Services</a>
+  <a href="/assessment_db/pages/bookings_list.php">Bookings</a>
+  <a href="/assessment_db/pages/tools_list_assign.php">Tools</a>
+  <a href="/assessment_db/pages/payments_list.php">Payments</a>
 </nav>
